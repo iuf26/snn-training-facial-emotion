5d6ce4306b9f9dafc75bf9e73f79a9b578545da3
Iulia Filimon <iulia.filimon@ontegra.com>
2025-02-19 17:00:56 +0200

    removed zip folder
    Former-commit-id: 4f246bd031104c44d417077c526e16db9d26f2f2

--- csnn-simulator-build/CMakeFiles/CSNNS.dir/src/SparseTensor.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CSNNS.dir/src/SparseTensor.cpp.o: \
- /home/iulia/project2/csnn-simulator/src/SparseTensor.cpp \
- /usr/include/stdc-predef.h
--- csnn-simulator-build/CMakeFiles/CSNNS.dir/src/dataset/FaceMotor.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/CSNNS.dir/src/dataset/FaceMotor.cpp.o: \
- /home/iulia/project2/csnn-simulator/src/dataset/FaceMotor.cpp \
- /usr/include/stdc-predef.h \
- /home/iulia/project2/csnn-simulator/include/dataset/FaceMotor.h
